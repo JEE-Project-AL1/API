918d67c94d2f4f094128366a49720144d1995093
Yacine-Zegrour <48019265+Yacine-Zegrour@users.noreply.github.com>
2022-05-31 16:16:25 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@ git pull
 cd apijee
 mvn clean install 
 cd target
-kill $(ps aux | grep "jar codeup-0.0.1-" | grep -v "grep" | head -1 | awk '{print $2}')
+kill $(ps aux | grep "jar API-JEE-0.0.1-" | grep -v "grep" | head -1 | awk '{print $2}')
 nohup java -Dspring.profiles.active=dev -jar API-JEE-0.0.1-SNAPSHOT.jar &
